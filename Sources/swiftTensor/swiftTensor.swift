fa58bd7c0ecc22bd17b8aa064d224e54a411a114
DeKobus <machineko@protonmail.com>
2024-08-16 22:20:18 +0200

    remove CPU storage again

--- Sources/swiftTensor/swiftTensor.swift
+++ Sources/swiftTensor/swiftTensor.swift
@@ -37,21 +37,21 @@ public enum tensorOperations {
 //}
 //
 
-public protocol CPUDatatType {}
+// public protocol CPUDatatType {}
 
-extension Float32: CPUDatatType {}
-extension Float64: CPUDatatType {}
-extension Int32: CPUDatatType {}
+// extension Float32: CPUDatatType {}
+// extension Float64: CPUDatatType {}
+// extension Int32: CPUDatatType {}
 
 
-public struct CPUStorage<S: CPUDatatType> {
-   var gradient: Array<S>?
-   var data: Array<S>?
-}
+// public struct CPUStorage<S: CPUDatatType> {
+//    var gradient: Array<S>?
+//    var data: Array<S>?
+// }
 
-public struct CPU<S: CPUDatatType>: TenosrType {
-   public typealias StorageType = CPUStorage<S>
-}
+// public struct CPU<S: CPUDatatType>: TenosrType {
+//    public typealias StorageType = CPUStorage<S>
+// }
 
 public extension Tensor {
     var numberOfElements: Int {
